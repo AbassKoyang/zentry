170d1e0729be11fe0fdc3daa345237472832de8b
Abass Koyang <abasskoyang12345@gmail.com>
2025-01-10 23:49:56 +0100

    added About section, Navigation bar and some reusable components

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,16 @@
-import React from 'react'
-import Hero from './components/Hero'
-import { useGSAP } from '@gsap/react';
-import gsap from 'gsap';
+import React from 'react';
+import Hero from './components/Hero';
+import About from './components/About';
+import NavBar from './components/NavBar';
 
 const App = () => {
-  useGSAP(( )=> {
-    gsap.from('.boxes', {
-      width: 0,
-      height:  0,
-      opacity: 0,
-      duration: 1,
-      repeat: -1,
-      yoyo: true,
-      stagger: {
-        amount: 1.5,
-        grid: [4,4], 
-        ease: 'none',
-        from: 'start'
-      }
-    })
-  })
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <NavBar />
       <Hero />
-      <section id='container' className='grid grid-cols-4 grid-rows-4 gap-3 min-h-screen bg-blue-500 z-0'>
-        <div className="boxes col-span-1 row-span-1 bg-green-500 opacity-100"></div>
-        <div className="boxes col-span-1 row-span-1 bg-green-500 opacity-100"></div>
-        <div className="boxes col-span-1 row-span-1 bg-green-500 opacity-100"></div>
-        <div className="boxes col-span-1 row-span-1 bg-green-500 opacity-100"></div>
-        <div className="boxes col-span-1 row-span-1 bg-green-500 opacity-100"></div>
-        <div className="boxes col-span-1 row-span-1 bg-green-500 opacity-100"></div>
-        <div className="boxes col-span-1 row-span-1 bg-green-500 opacity-100"></div>
-        <div className="boxes col-span-1 row-span-1 bg-green-500 opacity-100"></div>
-        <div className="boxes col-span-1 row-span-1 bg-green-500 opacity-100"></div>
-        <div className="boxes col-span-1 row-span-1 bg-green-500 opacity-100"></div>
-        <div className="boxes col-span-1 row-span-1 bg-green-500 opacity-100"></div>
-        <div className="boxes col-span-1 row-span-1 bg-green-500 opacity-100"></div>
-        <div className="boxes col-span-1 row-span-1 bg-green-500 opacity-100"></div>
-        <div className="boxes col-span-1 row-span-1 bg-green-500 opacity-100"></div>
-        <div className="boxes col-span-1 row-span-1 bg-green-500 opacity-100"></div>
-        <div className="boxes col-span-1 row-span-1 bg-green-500 opacity-100"></div>
-      </section>
+      <About />
     </main>
   )
 }
 
-export default App
+export default App;
